27bc969e8251154e3137d67f4b656557c1b889c7
ayushgoyal7796 <ayushgoyal7796@gmail.com>
2020-06-26 12:59:16 +0530

    update video_description

--- lib/screens/home/widgets/video_description.dart
+++ lib/screens/home/widgets/video_description.dart
@@ -4,25 +4,35 @@ class VideoDescription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Container(
-            height: 10.0,
-            color: Colors.green[300],
-            margin: EdgeInsets.only(top: 10.0),
-          ),
-          Container(
-            height: 10.0,
-            color: Colors.green[300],
-            margin: EdgeInsets.only(top: 10.0),
-          ),
-          Container(
-            height: 10.0,
-            color: Colors.green[300],
-            margin: EdgeInsets.only(top: 10.0),
-          ),
-        ],
+      child: Container(
+        height: 70.0,
+        padding: EdgeInsets.only(left: 20.0),
+        decoration: BoxDecoration(
+          border: Border.all(),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '@agoyal007',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text('Video title and some other stuff'),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.music_note,
+                  size: 15.0,
+                ),
+                Text(
+                  'Artist name - Album name - song',
+                  style: TextStyle(fontSize: 12.0),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
