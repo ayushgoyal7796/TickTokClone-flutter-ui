7ddd9a102f598e08a1f4c9156332a5b74811656c
ayushgoyal7796 <ayushgoyal7796@gmail.com>
2020-06-26 20:52:18 +0530

    update top_section

--- lib/screens/home/widgets/top_section.dart
+++ lib/screens/home/widgets/top_section.dart
@@ -5,7 +5,20 @@ class TopSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 100.0,
-      color: Colors.yellow[300],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text('Following'),
+          SizedBox(
+            width: 15.0,
+          ),
+          Text(
+            'For you',
+            style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
     );
   }
 }
